cb266bc1ed5c7a670df10a94ed3b29f3ebb99c5b
Michael Roberts <michaelroberts9000@gmail.com>
2021-11-20 13:17:07 -0500

    update some ad urls from ads to ad

--- ads/urls.py
+++ ads/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name='ads'
 urlpatterns = [
     path('', views.AdListView.as_view(), name='all'),
-    path('ads/<int:pk>', views.AdDetailView.as_view(), name='ad_detail'),
-    path('ads/create',
+    path('ad/<int:pk>', views.AdDetailView.as_view(), name='ad_detail'),
+    path('ad/create',
         views.AdCreateView.as_view(success_url=reverse_lazy('ads:all')), name='ad_create'),
-    path('ads/<int:pk>/update',
+    path('ad/<int:pk>/update',
         views.AdUpdateView.as_view(success_url=reverse_lazy('ads:all')), name='ad_update'),
     path('ads/<int:pk>/delete',
         views.AdDeleteView.as_view(success_url=reverse_lazy('ads:all')), name='ad_delete'),
